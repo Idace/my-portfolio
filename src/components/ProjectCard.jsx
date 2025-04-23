d636b2bc56c963be3c4e31922ea2d3154449efc8
Idace <idaebeling@gmail.com>
2025-04-23 16:38:46 +0200

    First commit

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {FaGithub, FaCode, FaPuzzlePiece, FaUserTie, FaLightbulb
+}
+from "react-icons/fa";
+
+import "./ProjectCard.css";
+
+function ProjectCard({icon, name, screenshot,github, tech, description, whatItDoes, learned, role,challenges }){
+    return (
+        <div className="card">
+            <div className="icon-hero">{icon}</div>
+            <img src={screenshot} alt={`${name} screenshot`} className="card-image"/>
+            <div className="card-content">
+                <h2>{name}</h2>
+                <p>{description}</p>
+                    <p><FaCode className="icon"/> <strong className="label">Tech:</strong> {tech}</p>
+                    <p><FaLightbulb className="icon"/> <strong className="label">What it does:</strong> {whatItDoes}</p>
+                    <p><FaPuzzlePiece className="icon"/> <strong className="label">What I learned:</strong> {learned}</p>
+                    <p><FaUserTie className="icon"/> <strong className="label">Role:</strong> {role}</p>
+                    <p><FaPuzzlePiece className="icon"/><strong className="label">Challenges:</strong> {challenges}</p>
+                < a href={github} target="_blank" rel="noopener noreferrer" className="github-link">
+                    <FaGithub /> View on Github
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectCard;
